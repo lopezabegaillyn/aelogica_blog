9664aa63b841646975bb08c0a484b4f5ed3568a2
Abegail Lopez <lopezabegaillyn@gmail.com>
2017-06-19 09:15:33 +0800

    Login and Signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
     get 'welcome/index'
     
     resources :articles do
@@ -6,6 +8,15 @@ Rails.application.routes.draw do
     end
     
     root 'welcome#index'
+    
+   
+    post '/signup', to: 'users#create'
+    get    '/login',   to: 'sessions#new'
+    post   '/login',   to: 'sessions#create'
+    delete '/logout',  to: 'sessions#destroy'
+  
+    resources :users    
+    
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
